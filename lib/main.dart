75341c153958499c966d0d886fef5562c19eaec1
Sitam Sardar <sitam.dir@gmail.com>
2023-04-06 20:06:56 +0530

    Grouped List Implemented

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,16 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_ui_practice/providers/count_down_provider.dart';
-import 'package:flutter_ui_practice/providers/count_up_provider.dart';
-import 'package:flutter_ui_practice/ui/pages/dummy_checking_page.dart';
-import 'package:flutter_ui_practice/ui/pages/movie_list_page.dart';
-import 'package:flutter_ui_practice/viewmodels/movie_list_viewmodel.dart';
-import 'package:provider/provider.dart';
+import 'package:flutter_ui_practice/pages/list_page.dart';
 
 void main() => runApp(App());
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: "Movies MVVM Example",
+        title: "Grouped List Demo",
         debugShowCheckedModeBanner: false,
         theme: ThemeData(primarySwatch: Colors.teal),
-        home: MultiProvider(
-          providers: [
-            ChangeNotifierProvider(create: (_) => MovieListViewModel()),
-            ChangeNotifierProvider(create: (_) => CountUpProvider()),
-            ChangeNotifierProvider(create: (_) => CountDownProvider()),
-          ],
-          child: DummyCheckingPage(),
-        )
-        // ChangeNotifierProvider(
-        //   create: (context) => MovieListViewModel(),
-        //   child: DummyCheckingPage(),
-        // )
+        home: GroupedListPage()
     );
   }
 }
---
+++ lib/pages/list_page.dart
@@ -0,0 +1,51 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_ui_practice/utils/random_name_generator.dart';
+
+class GroupedListPage extends StatefulWidget{
+  const GroupedListPage({super.key});
+
+  @override
+  State<GroupedListPage> createState() => _GroupedListPageState();
+}
+
+class _GroupedListPageState extends State<GroupedListPage> {
+  late RandomNameGenerator nameGenerator;
+  Map<String, List<String>>? groupedMap;
+  @override
+  void initState() {
+    nameGenerator = RandomNameGenerator();
+    groupedMap = nameGenerator.groupNamesMap;
+    super.initState();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Grouped List Demo"),
+      ),
+      body: Container(
+         child: ListView.separated(
+           padding: const EdgeInsets.all(8),
+           itemCount: groupedMap?.length ?? 0,
+           itemBuilder: (BuildContext context, int index1) {
+             return ListView.builder(
+               itemCount: groupedMap?.values.elementAt(index1).length ?? 0,
+               physics: NeverScrollableScrollPhysics(),
+               shrinkWrap: true,
+               itemBuilder: (BuildContext context, int index2){
+                 return Container(
+                   height: 50,
+                   color: Colors.redAccent,
+                   child: Center(child: Text('${groupedMap?.values.elementAt(index1).elementAt(index2)}')),
+                 );
+               },
+             );
+           },
+           separatorBuilder: (BuildContext context, int index) => Text("${groupedMap?.keys.elementAt(index)}"),
+         )
+      ),
+    );
+  }
+}
