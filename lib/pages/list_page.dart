67380cb8a0ae786925626f9f3db72eaece63f9d4
Sitam Sardar <sitam.dir@gmail.com>
2023-04-07 12:58:32 +0530

    Expandable List Feature Added

--- lib/pages/list_page.dart
+++ lib/pages/list_page.dart
@@ -1,9 +1,8 @@
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_ui_practice/utils/random_name_generator.dart';
 
-class GroupedListPage extends StatefulWidget{
+class GroupedListPage extends StatefulWidget {
   const GroupedListPage({super.key});
 
   @override
@@ -13,12 +12,15 @@ class GroupedListPage extends StatefulWidget{
 class _GroupedListPageState extends State<GroupedListPage> {
   late RandomNameGenerator nameGenerator;
   Map<String, List<String>>? groupedMap;
+  String? clickedHeader;
+
   @override
   void initState() {
     nameGenerator = RandomNameGenerator();
     groupedMap = nameGenerator.groupNamesMap;
     super.initState();
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,30 +28,44 @@ class _GroupedListPageState extends State<GroupedListPage> {
         title: const Text("Grouped List Demo"),
       ),
       body: Container(
-         child: ListView.builder(
-           padding: const EdgeInsets.all(8),
-           itemCount: groupedMap?.length ?? 0,
-           itemBuilder: (BuildContext context, int index1) {
-             return Column(
-               children: [
-                 Text("${groupedMap?.keys.elementAt(index1)}"),
-                 ListView.builder(
-                 itemCount: groupedMap?.values.elementAt(index1).length ?? 0,
-                 physics: NeverScrollableScrollPhysics(),
-                 shrinkWrap: true,
-                 itemBuilder: (BuildContext context, int index2){
-                   return Container(
-                     height: 50,
-                     color: Colors.redAccent,
-                     child: Center(child: Text('${groupedMap?.values.elementAt(index1).elementAt(index2)}')),
-                   );
-                 },
-               )
-               ],
-             );
-           },
-         )
-      ),
+          child: ListView.builder(
+        padding: const EdgeInsets.all(8),
+        itemCount: groupedMap?.length ?? 0,
+        itemBuilder: (BuildContext context, int index1) {
+          var key = groupedMap?.keys.elementAt(index1);
+          return Column(
+            children: [
+              InkWell(
+                  onTap: () {
+                    clickedHeader = key;
+                    setState(() {});
+                  },
+                  child: Container(
+                    height: 50,
+                    color: Colors.amber,
+                    child: Center(child: Text('$key')),
+                  )),
+              clickedHeader == key
+                  ? ListView.builder(
+                      itemCount:
+                          groupedMap?.values.elementAt(index1).length ?? 0,
+                      physics: NeverScrollableScrollPhysics(),
+                      shrinkWrap: true,
+                      itemBuilder: (BuildContext context, int index2) {
+                        return Container(
+                          height: 50,
+                          color: Colors.redAccent,
+                          child: Center(
+                              child: Text(
+                                  '${groupedMap?.values.elementAt(index1).elementAt(index2)}')),
+                        );
+                      },
+                    )
+                  : SizedBox()
+            ],
+          );
+        },
+      )),
     );
   }
 }
