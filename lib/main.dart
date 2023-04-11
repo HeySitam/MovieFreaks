d2b82bd6ad4a3550a97cad051ac6879188e598d2
Sitam Sardar <sitam.dir@gmail.com>
2023-04-11 17:49:58 +0530

    Started Working on Round Round Animation

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_ui_practice/pages/list_page.dart';
-import 'package:flutter_ui_practice/providers/header_selection_provider.dart';
-import 'package:provider/provider.dart';
 
 void main() => runApp(App());
 class App extends StatelessWidget {
@@ -12,10 +9,7 @@ class App extends StatelessWidget {
         title: "Grouped List Demo",
         debugShowCheckedModeBanner: false,
         theme: ThemeData(primarySwatch: Colors.teal),
-        home: ChangeNotifierProvider(
-          create: (_) => HeaderSelectionProvider(),
-            child: GroupedListPage()
-        )
+        home: Container()
     );
   }
 }
---
+++ lib/pages/animation_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+
+class MainAnimationPage extends StatefulWidget{
+  @override
+  State<MainAnimationPage> createState() => _MainAnimationPageState();
+}
+
+class _MainAnimationPageState extends State<MainAnimationPage> {
+  @override
+  Widget build(BuildContext context) {
+     return Stack();
+  }
+}
