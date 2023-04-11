f1cabe8350d51306315484a7a563b5ed5c1fd6f7
Sitam Sardar <sitam.dir@gmail.com>
2023-04-11 13:52:48 +0530

    toggling and bottom scrolling fixed in the named_group

--- lib/providers/header_selection_provider.dart
+++ lib/providers/header_selection_provider.dart
@@ -2,9 +2,17 @@ import 'package:flutter/cupertino.dart';
 
 class HeaderSelectionProvider extends ChangeNotifier{
   String? _selectedHeader;
+  bool _shouldExpand = false;
   get getSelectedHeader => _selectedHeader;
+  get getShouldExpand => _shouldExpand;
+
   setSelectedHeader(String selectedHeader){
     _selectedHeader = selectedHeader;
+    notifyListeners();
+  }
+
+  setShouldExpand(bool shouldExpandInfo){
+    _shouldExpand = shouldExpandInfo;
     notifyListeners();
   }
 }
--- lib/widgets/list_page_item.dart
+++ lib/widgets/list_page_item.dart
@@ -14,49 +14,64 @@ class ListPageItem extends StatefulWidget {
 }
 
 class _ListPageItemState extends State<ListPageItem> {
+  bool _isExpanded = false;
+
   @override
   Widget build(BuildContext context) {
     final selectionProvider =
         Provider.of<HeaderSelectionProvider>(context, listen: false);
-    print('build');
-    return Container(
-          child: Column(
-            children: [
-              InkWell(
-                  onTap: () {
-                    selectionProvider.setSelectedHeader(widget.headerKey);
-                  },
-                  child: Card(
-                    child: Container(
-                      height: 50,
-                      color: Colors.amber,
-                      child: Center(child: Text(widget.headerKey)),
-                    ),
-                  )),
-              AnimatedSize(
-                duration: Duration(milliseconds: 600),
-                child: Consumer<HeaderSelectionProvider>(
-                    builder: (context, provider, child) {
-                  return Visibility(
-                    visible: provider.getSelectedHeader == widget.headerKey,
-                    child: ListView.builder(
-                      itemCount: widget.nameList.length ?? 0,
-                      physics: NeverScrollableScrollPhysics(),
-                      shrinkWrap: true,
-                      itemBuilder: (BuildContext context, int index) {
-                        return Container(
-                          height: 50,
-                          color: Colors.redAccent,
-                          child: Center(
-                              child: Text(widget.nameList.elementAt(index))),
-                        );
-                      },
-                    ),
-                  );
-                }),
-              ),
-            ],
+    print('build HEre');
+    return Card(
+      child: Column(
+        children: [
+          InkWell(
+            onTap: () {
+              bool shouldToggleCard =
+                  selectionProvider.getSelectedHeader == widget.headerKey;
+              if (shouldToggleCard) {
+                selectionProvider
+                    .setShouldExpand(!selectionProvider.getShouldExpand);
+              }
+              selectionProvider.setSelectedHeader(widget.headerKey);
+            },
+            child: Container(
+              height: 50,
+              color: Colors.amber,
+              child: Center(child: Text(widget.headerKey)),
+            ),
           ),
-        );
+
+          Consumer<HeaderSelectionProvider>(
+              builder: (context, provider, child) {
+            bool _shouldExpand =
+                provider.getSelectedHeader == widget.headerKey &&
+                    provider.getShouldExpand;
+            return AnimatedContainer(
+              height: _shouldExpand ? widget.nameList.length * 50 : 0,
+              duration: _shouldExpand
+                  ? Duration(milliseconds: 600)
+                  : Duration(milliseconds: 0),
+              child: Visibility(
+                visible: provider.getSelectedHeader == widget.headerKey,
+                child: ListView.builder(
+                  itemCount: widget.nameList.length ?? 0,
+                  physics: NeverScrollableScrollPhysics(),
+                  //  shrinkWrap: true,
+                  itemBuilder: (BuildContext context, int index) {
+                    return Container(
+                      height: 50,
+                      color: Colors.redAccent,
+                      child:
+                          Center(child: Text(widget.nameList.elementAt(index))),
+                    );
+                  },
+                ),
+              ),
+            );
+          }),
+          //  ),
+        ],
+      ),
+    );
   }
 }
