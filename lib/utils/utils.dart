78be1d763436cbe76dabb8289934c57ab7ebcfd5
Sitam Sardar <sitamadex11@gmail.com>
2025-01-04 22:09:17 +0530

    Update utils.dart

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 class Utils{
   static const baseUrl = "https://api.themoviedb.org/3/";
   static const baseImgUrl = "http://image.tmdb.org/t/p/w500";
-  static const api_key = "4ec53f3b9ac13226645942eca37f3df0";
+  static const api_key = "add_your_api_key";
   static const background_color = Color(0xFF03091c);
   static String baseYoutubeUrl = "https://www.youtube.com/watch?v=";
 }
